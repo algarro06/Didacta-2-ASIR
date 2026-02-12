f28824321ef861a55a092d300ca9bcdb11a47af1
aleexjo12 <alom444@g.educaand.es>
2026-02-12 13:35:37 +0100

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+session_destroy();
+
+foreach ($_COOKIE as $key => $value) {
+    setcookie($key, '', time() - 3600, "/");
+}
+
+header("Location: login.php");
+exit();
+?>
