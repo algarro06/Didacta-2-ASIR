09eb4d29773f712ebde84c0b9a6186fe2d1463d6
ivanortigosa <garethivanobalic@gmail.com>
2026-06-06 12:43:26 +0200

    Subida Final

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -16,7 +16,7 @@
     @if (!str_contains($current, 'principal'))
         <div class="user-actions animacion">
             <a href="{{ url('/home') }}" class="back-btn">
-                <-- Volver al panel
+                ← Volver al panel
             </a>
         </div>
     @endif
